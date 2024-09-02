28ea65d5a7fc959c1072d05c59e97fbb6edf2218
NoobAndCoding <just.gaming554t644@gmail.com>
2024-09-02 15:36:37 +0300

    added a function
    added a function in JS and added id to h2

--- sideJS.js
+++ sideJS.js
@@ -7,3 +7,8 @@ function closeNav() {
     document.getElementById("sideMenu").style.width = "0px";
     document.getElementById("contentArea").style.marginLeft = "0px"
 }
+
+function showContent(content) {
+    document.getElementById("contentTitle").textContent = content + " Page";
+    closeNav()
+}
